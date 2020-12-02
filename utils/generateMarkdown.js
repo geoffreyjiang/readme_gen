9ca11ec53e806a7aef832692d42a8e9f98261b02
Geoffrey Jiang <geoffreychjiang@gmail.com>
2020-12-02 13:30:14 -0800

    connect responses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+//Title
+`# ${response.title}
 
-`;
+## Description
+${response.description}`
+
+
+//Installations
+`## Installation:
+${response.installation}`
+
+//Usage
+`## Usage:
+${response.usage}`
+
+//Contribution
+`## Contribution
+${response.contributors}`
+
+//License
+`## License
+${response.license}`
+
+
+//Tests
+`## Tests
+${response.tests}`
+
+//Questions
+`## Questions
+${response.contact}`
+
+
+return response
 }
+
 
 module.exports = generateMarkdown;
